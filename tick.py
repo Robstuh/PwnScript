e909048cb2308bee3c2523bda5b1c371e97be53d
Robbie <r0bb13@null.net>
2012-01-12 21:36:20 +0000

    Tick script original

---
+++ tick.py
@@ -0,0 +1,18 @@
+#Engine by Robbie
+from org.pwnxile.core import Script
+
+def world_tick(engine):
+	engine.getItemHandler().tick()
+	engine.getShopHandler().tick()
+	engine.getObjectHandler().tick()
+	engine.getObjectManager().tick()
+	
+def entity_updating(engine):	
+	engine.getNpcHandler().tick()
+	engine.getPlayerHandler().tick()
+
+def game_tick(engine):
+	engine.getCastleWars().tick()
+	engine.getPestContol().tick()
+	engine.getFightPits().tick()
+	
