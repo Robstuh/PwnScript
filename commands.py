01157269ac156ba5886f4eeceed3d7ede4ab7d0e
Robbie <r0bb13@null.net>
2012-02-26 13:53:48 +0000

    New commands

---
+++ commands.py
@@ -0,0 +1,17 @@
+from org.pwnxile.core import Script
+from org.pwnxile.rs2 import PlayerHandler
+from org.pwnxile.rs2.content import Staff
+
+def command_staff(player):
+	player.sendMessage("There are " + str(Staff.getStaffCount()) + " staff online.")
+	player.sendMessage("They are:")
+	player.sendMessage(str(Staff.getStaffNames()))
+	player.sendMessage("Send one a message for help!")
+	
+def command_players(player):
+	player.sendMessage("There are currently " + str(PlayerHandler.getPlayerCount()) + " players online.")
+	
+def command_printstaff(player):
+	for value in Staff.getStaffList():
+		player.sendMessage(value.playerName)
+				
