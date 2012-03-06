6e6705110826ee443ada1e001dea331e9728984d
Robbie <r0bb13@null.net>
2012-03-06 14:35:48 +0000

    Additions

--- chicken_killer.py
+++ chicken_killer.py
@@ -6,8 +6,3 @@ def npckill_41(killer):
 	else:
 		killer.sendMessage("You receive 1 PxP for killing 10 chickens.")
 		killer.chickenKills = 0
-		
-def takeNuts(player):
-	player.startAnimation(2292)
-	player.addItem(4012)
-	player.sendMessage("You pick some monkey nuts from the bush.")
