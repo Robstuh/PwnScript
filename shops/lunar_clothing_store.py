aab6ced7dc856aef5c65188bef11d1a13ba3b510
Robbie <r0bb13@null.net>
2013-01-18 01:51:46 +0000

    lunar shops

---
+++ shops/lunar_clothing_store.py
@@ -0,0 +1,11 @@
+lunar_clothing_store = Shop("Lunar Clothing Store", 46)
+lunar_clothing_store.addItem(ShopItem(9084, 1))
+lunar_clothing_store.addItem(ShopItem(9097, 1))
+lunar_clothing_store.addItem(ShopItem(9098, 1))
+lunar_clothing_store.addItem(ShopItem(9099, 1))
+lunar_clothing_store.addItem(ShopItem(9100, 1))
+lunar_clothing_store.addItem(ShopItem(9101, 1))
+lunar_clothing_store.addItem(ShopItem(9102, 1))
+
+def second_click_npc_4513(player):
+	player.getShop().openShop(46)
---
+++ shops/lunar_general.py
@@ -0,0 +1,5 @@
+lunar_general_store = Shop("General Store", 47)
+lunar_general_store.sell = 1
+
+def second_click_npc_4516(player):
+	player.getShop().openShop(47)
