b2a7696beb3c17ef19be6e36f065eac65004580c
Robbie <r0bb13@null.net>
2013-01-21 15:35:54 +0000

    shop dialogues

--- shops/lunar_general.py
+++ shops/lunar_general.py
@@ -1,5 +1,20 @@
 lunar_general_store = Shop("General Store", 47)
 lunar_general_store.sell = 1
 
+def first_click_npc_4516(player):
+	player.startChat(5503)
+
 def second_click_npc_4516(player):
 	player.getShop().openShop(47)
+	
+def chat_5503(player):
+	player.playerChat("Do you have anything for sale?")
+	player.nextChat(5504)
+	
+def chat_5504(player):
+	player.npcChat("Yes I sell various items and am also", "willing to purchase items from players", "for a good price.")
+	player.nextChat(5505)
+	
+def chat_5505(player):
+	player.getShop().openShop(47)
+	player.endChat()
